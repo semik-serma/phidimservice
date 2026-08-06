3e037b518138c42ba43170ee8a3233f7189c6974
semik-serma <semikserma@gmail.com>
2026-08-06 06:39:31 +0545

    updated website

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,16 +18,26 @@ export const metadata = {
   },
   description:
     "Professional on-site technical services in Phidim, Panchthar — DTH, CCTV, electrical, computer repair, plumbing and more.",
-  icons: {
+  // icons: {
+  //   icon: [
+  //     { url: "/logo.png", type: "image/png" },
+  //     { url: "/icon.png", type: "image/png" }
+  //   ],
+  //   shortcut: ["/logo.png"],
+  //   apple: [
+  //     { url: "/apple-icon.png", type: "image/png" }
+  //   ]
+  // },
+   icons: {
     icon: [
-      { url: "/logo.png", type: "image/png" },
-      { url: "/icon.png", type: "image/png" }
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
-    shortcut: ["/logo.png"],
-    apple: [
-      { url: "/apple-icon.png", type: "image/png" }
-    ]
+    apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
+  
   openGraph: {
     title: "Phidim Service | Technical & Digital Services",
     description: "Professional on-site technical services in Phidim, Panchthar — DTH, CCTV, electrical, computer repair, plumbing and more.",
